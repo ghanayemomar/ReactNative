eda6b983697d5135e70de3633694a8a5c096931a
gem00n <o.ghanaiem@student.aaup.edu>
2023-04-30 01:38:38 +0300

    21. Improving The Layout

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default function App() {
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} placeholder="Your Course goal!" />
-        <Button title="Add Goal" />
+        <Button title="Add Goal!" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List Of Goals ...</Text>
       </View>
     </View>
@@ -16,17 +16,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex:1,
+    paddingTop:50,
+    paddingHorizontal:16
   },
   inputContainer: {
+    flex:1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth:1,
+    borderBottomColor:"#cccccc"
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 10,
     padding: 10,
   },
+  goalsContainer:{
+    flex:5,
+  }
 });
