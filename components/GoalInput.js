1f54d409330e9cf02af04e51bf0a96f9c2c30518
gem00n <o.ghanaiem@student.aaup.edu>
2023-05-04 01:09:52 +0300

    34. Styling the Modal Overlay

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, TextInput, Button, Modal} from "react-native";
+import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
 import { useState } from "react";
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -13,15 +13,22 @@ function GoalInput(props) {
   }
   return (
     <Modal visible={props.visible} animationType="slide">
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Enter your course goal!"
-        onChangeText={goalInputHandler}
-        value={enteredGoalText}
-      />
-      <Button title="Add Goal" onPress={addGoalHandler} />
-    </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter your course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={() => {}} />
+          </View>
+        </View>
+      </View>
     </Modal>
   );
 }
@@ -30,8 +37,8 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    padding: 16,
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
@@ -40,8 +47,14 @@ const styles = StyleSheet.create({
   textInput: {
     borderWidth: 2,
     borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 10,
+    width: "100%",
     padding: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+  },
+  button: {
+    width: "30%",
+    marginHorizontal: 10,
   },
 });
